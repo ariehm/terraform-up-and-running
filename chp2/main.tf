94e2d2f04b5245c5780972ad5edfca5c926f0426
Andrew Riehm <ariehm@vulncheck.com>
2026-01-29 18:16:36 -0700

    WIP: tried to add the next step, not working
    I've got the web server script thing, and the security group, but when I deploy
    it, I can't access the resulting IP address.  Not sure if there's an
    account-wide security thing in place that might be blocking it or what...

--- chp2/main.tf
+++ chp2/main.tf
@@ -9,4 +9,23 @@ resource "aws_instance" "example" {
     Testing = "VeryYes"
     Name    = "AndrewTest"
   }
+
+  user_data                   = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.xhtml
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  user_data_replace_on_change = true
+
+  vpc_security_group_ids = [aws_security_group.instance.id]
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
