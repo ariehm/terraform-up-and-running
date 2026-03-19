66016f54e6cd4d26fd9fcd51950e744827156098
Andrew Riehm <ariehm@vulncheck.com>
2026-03-19 09:25:49 -0600

    refactor(chp4): separate things into module/ and live/ folders

--- chp4/live/stage/services/webserver-cluster/main.tf
+++ chp4/live/stage/services/webserver-cluster/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "../../../../modules/services/webserver-cluster"
 
   cluster_name           = "h4tchtest-stage"
   db_remote_state_bucket = "h4tch-test-terraform-up-and-running-state"
